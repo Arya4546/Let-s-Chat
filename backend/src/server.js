0184332c15e91a754a16f9779b0f61086210eb97
Arya Deep Singh <singharya9693@gmail.com>
2025-06-07 13:04:28 +0530

    Update server.js
    added frontend url for cors configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://let-s-chat-mocha.vercel.app",
     credentials: true
 }));
 
